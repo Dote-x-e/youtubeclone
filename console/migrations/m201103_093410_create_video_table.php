5a9b057f83259ab1f2aa32bf9d6e920f50e2bf89
Dote-x-e <mwiti.k.gitonga@gmail.com>
2020-11-03 15:02:30 +0300

    first commit

---
+++ console/migrations/m201103_093410_create_video_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%video}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%user}}`
+ */
+class m201103_093410_create_video_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%video}}', [
+            'id' => $this->primaryKey(),
+            'video_id' => $this->string(16)->notNull(),
+            'title' => $this->string(512)->notNull(),
+            'description' => $this->text()(),
+            'created_by' => $this->integer(11),
+        ]);
+
+        // creates index for column `created_by`
+        $this->createIndex(
+            '{{%idx-video-created_by}}',
+            '{{%video}}',
+            'created_by'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-video-created_by}}',
+            '{{%video}}',
+            'created_by',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            '{{%fk-video-created_by}}',
+            '{{%video}}'
+        );
+
+        // drops index for column `created_by`
+        $this->dropIndex(
+            '{{%idx-video-created_by}}',
+            '{{%video}}'
+        );
+
+        $this->dropTable('{{%video}}');
+    }
+}
